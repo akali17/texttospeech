eefff54b18333cd21f270b2a1f6dd29c3d56704f
akali17 <2251120334@ut.edu.vn>
2025-05-15 00:10:27 +0900

    sua tts.py

--- code/tts.py
+++ code/tts.py
@@ -3,19 +3,3 @@ from gtts import gTTS
 def text_to_speech(text, lang='en'):
     tts = gTTS(text=text, lang=lang)
     return tts
-
-
-### file_utils.py
-from PyQt5.QtWidgets import QFileDialog
-
-def save_text_file(text):
-    path, _ = QFileDialog.getSaveFileName(None, "Lưu văn bản", "output.txt", "Text Files (*.txt)")
-    if path:
-        with open(path, "w", encoding="utf-8") as file:
-            file.write(text)
-
-
-def save_audio_file(tts_obj):
-    path, _ = QFileDialog.getSaveFileName(None, "Lưu âm thanh", "output.mp3", "MP3 Files (*.mp3)")
-    if path:
-        tts_obj.save(path)
